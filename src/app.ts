d342cb60513587b7bc7729fae851b658b47effac
Yannick Mayeur <yannick.mayeur@gmail.com>
2019-10-23 15:20:58 +0200

    Change app.js to src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-const express = require('express');
+import express from 'express';
 const app = express();
-const port = 3000;
+const port: number = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
